47475c6d7820c107c8541e77c3c4300c21ca573b
Andrius Merkys <andrius.merkys@gmail.com>
2023-03-09 09:52:07 +0200

    Prepare to transform coordinates to orthogonal from fractional.

--- lib/Chemistry/File/QuantumESPRESSO.pm
+++ lib/Chemistry/File/QuantumESPRESSO.pm
@@ -5,6 +5,8 @@ package Chemistry::File::QuantumESPRESSO;
 
 use base 'Chemistry::File';
 use Chemistry::Mol;
+use Math::MatrixReal;
+use Math::Trig;
 use Math::VectorReal qw(vector);
 
 use strict;
@@ -38,11 +40,32 @@ sub parse_string {
         }
     }
 
+    if( @cell_vectors ) {
+        my @lengths = map { $_->length } @cell_vectors;
+        my $alpha = acos( ($cell_vectors[1] . $cell_vectors[2]) / ($lengths[1] * $lengths[2]) );
+        my $beta  = acos( ($cell_vectors[0] . $cell_vectors[2]) / ($lengths[0] * $lengths[2]) );
+        my $gamma = acos( ($cell_vectors[0] . $cell_vectors[1]) / ($lengths[0] * $lengths[1]) );
+    }
+
     for my $atom (@atoms) {
         $mol->new_atom( symbol => $atom->[0], coords => $atom->[1] );
     }
 
     return ( $mol );
+}
+
+# Taken from cod-tools, rev. 9257
+sub _symop_ortho_from_fract
+{
+    my ($a, $b, $c, $alpha, $beta, $gamma) = @_;
+    my ($ca, $cb, $cg) = map {cos} ($alpha, $beta, $gamma);
+    my $sg = sin($gamma);
+
+    return new_from_rows(
+        [ $a, $b*$cg, $c*$cb               ],
+        [  0, $b*$sg, $c*($ca-$cb*$cg)/$sg ],
+        [  0,      0, $c*sqrt($sg*$sg-$ca*$ca-$cb*$cb+2*$ca*$cb*$cg)/$sg ]
+    );
 }
 
 1;
