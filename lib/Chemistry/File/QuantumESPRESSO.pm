5b534b46d5286b51e1a1bdf7d83ddb332475ed46
Andrius Merkys <andrius.merkys@gmail.com>
2023-03-09 08:52:33 +0200

    Initial scaffold.

---
+++ lib/Chemistry/File/QuantumESPRESSO.pm
@@ -0,0 +1,16 @@
+package Chemistry::File::QuantumESPRESSO;
+
+# VERSION
+# $Id$
+
+use base 'Chemistry::File';
+use Chemistry::Mol;
+
+use strict;
+use warnings;
+
+sub parse_string {
+    my ($self, $s, %opts) = @_;
+}
+
+1;
