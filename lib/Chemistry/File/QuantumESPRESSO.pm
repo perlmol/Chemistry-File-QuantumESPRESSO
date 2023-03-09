f5fe156720a3ade5df245634dfcb6afe3daa9964
Andrius Merkys <andrius.merkys@gmail.com>
2023-03-09 09:39:58 +0200

    Read in atoms and construct a molecule.

--- lib/Chemistry/File/QuantumESPRESSO.pm
+++ lib/Chemistry/File/QuantumESPRESSO.pm
@@ -5,12 +5,43 @@ package Chemistry::File::QuantumESPRESSO;
 
 use base 'Chemistry::File';
 use Chemistry::Mol;
+use Math::VectorReal qw(vector);
 
 use strict;
 use warnings;
 
 sub parse_string {
     my ($self, $s, %opts) = @_;
+
+    my $mol_class  = $opts{mol_class}  || 'Chemistry::Mol';
+    my $atom_class = $opts{atom_class} || $mol_class->atom_class;
+    my $bond_class = $opts{bond_class} || $mol_class->bond_class;
+    local $_;
+
+    my $mol = $mol_class->new;
+
+    my @atoms;
+    my @cell_vectors;
+
+    my @lines = split "\n", $s;
+    while (@lines) {
+        my $line = shift @lines;
+        $line =~ s/^\s+//;
+        $line =~ s/\s+$//;
+
+        if(      $line =~ /^ATOMIC_POSITIONS crystal$/ ) {
+            my( $symbol, @coords ) = split /\s+/, $line;
+            push @atoms, [ $symbol, vector( @coords ) ];
+        } elsif( $line =~ /^CELL_PARAMETERS angstrom$/ ) {
+            @cell_vectors = ( shift @lines, shift @lines, shift @lines );
+        }
+    }
+
+    for my $atom (@atoms) {
+        $mol->new_atom( symbol => $atom->[0], coords => $atom->[1] );
+    }
+
+    return ( $mol );
 }
 
 1;
